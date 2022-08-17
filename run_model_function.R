ddb8ee9c651f8e0583afa0decb2205097617ce63
Francisco Jara <fcojara506@gmail.com>
2022-08-17 11:33:48 -0400

    delete images

--- run_model_function.R
+++ run_model_function.R
@@ -75,3 +75,71 @@ return(list(
   data_input = data
   ))
 }
+
+
+# from flow to volume
+forecast_flow =
+  function(month_target,
+           catchment_code = '5410002',
+           month_initialisation = "sep",
+           region = "ChileCentral_ens30avg",
+           wy_holdout = 1992
+           ) {
+    
+    data = preprocess_data(
+      catchment_code = catchment_code,
+      region = region,
+      month_initialisation = month_initialisation,
+      horizon_month_start = month_target,
+      horizon_month_end = month_target,
+      horizon_strategy = "fixed",
+      predictor_list =
+        c("pr_sum_-1months"),
+      wy_holdout = wy_holdout,
+      remove_wys = c(2020, 2021)
+    )
+    
+    # ensemble volume forecast
+    data_fore =
+      forecast_vol_ensemble(
+        data = data,
+        method = "lm",
+        #ridge, lm, rlm, simpls,
+        tuneLength = 10,
+        preProcess = c("center", "scale"),
+        n_members = 1000
+      )
+    
+    q_fore = data_fore$y_ens_fore
+    colnames(q_fore) = data$time_horizon$months_forecast_period
+    
+    return(data.frame(q_fore))
+  }
+
+flow_to_vol <- function(month_initialisation,...) {
+  
+  months_forecast_period =
+    forecast_horizon(month_initialisation,"dynamic")$
+    months_forecast_period
+  
+  # ensemble flow forecast for each month
+  q_fore = list()
+  for (month_forecast in months_forecast_period) {
+    q_fore[[month_forecast]] = 
+      forecast_flow(month_target = month_forecast,
+                    month_initialisation = month_initialisation,
+                    ...)
+  }
+  q_fore = do.call(cbind,q_fore)
+  
+  # ensemble volume from flow
+  y_ens_fore = apply(q_fore, 1, sum) %>%as.matrix()
+  
+  return(list(
+    q_fore = q_fore,
+    y_ens_fore = y_ens_fore
+  ))
+}
+
+a=flow_to_vol(month_initialisation = "sep")
+b=run_model()
